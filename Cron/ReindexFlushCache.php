3c86e1843900db5138de77ff99a072e2ffa6bd39
Jelle Siderius <jelle@hypershop.nl>
2022-10-31 10:49:04 +0100

    Added cronjob to reindex and flush caches

--- Cron/ReindexFlushCache.php
+++ Cron/ReindexFlushCache.php
@@ -5,21 +5,27 @@ declare(strict_types=1);
 namespace Hypershop\SpikePerformance\Cron;
 
 use Hypershop\SpikePerformance\Helper\Config;
+use Magento\Framework\App\Cache\Manager;
+use Magento\Indexer\Model\Indexer\CollectionFactory;
+use Magento\Indexer\Model\IndexerFactory;
 
 class ReindexFlushCache
 {
-    /**
-     * @var Config
-     */
     private $spikePerformanceConfig;
+    private $cacheManager;
+    private $indexerFactory;
+    private $collectionFactory;
 
-    /**
-     * @param Config $spikePerformanceConfig
-     */
     public function __construct(
-        Config $spikePerformanceConfig
+        Config $spikePerformanceConfig,
+        Manager $cacheManager,
+        IndexerFactory $indexerFactory,
+        CollectionFactory $collectionFactory
     ) {
         $this->spikePerformanceConfig = $spikePerformanceConfig;
+        $this->cacheManager = $cacheManager;
+        $this->indexerFactory = $indexerFactory;
+        $this->collectionFactory = $collectionFactory;
     }
 
     public function execute()
@@ -27,5 +33,17 @@ class ReindexFlushCache
         if (!$this->spikePerformanceConfig->getIsCronEnabled()) {
             return;
         }
+
+        // Reindex all indexes
+        $indexerCollection = $this->collectionFactory->create();
+        $indexIds = $indexerCollection->getAllIds();
+
+        foreach ($indexIds as $indexId) {
+            $index = $this->indexerFactory->create()->load($indexId);
+            $index->reindexAll();
+        }
+
+        // Flush all caches
+        $this->cacheManager->flush($this->cacheManager->getAvailableTypes());
     }
 }
